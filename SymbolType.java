48b84b3543b2a95fb2a444f7e4fff0ed47af11ac
Tiago Ribeiro <tiago7b27@gmail.com>
2019-04-09 11:57:11 +0100

    getType

--- SymbolType.java
+++ SymbolType.java
@@ -4,14 +4,28 @@ public class SymbolType{
     INT,
     INT_ARR,
     BOOLEAN,
-    VOID
+    VOID;
   }
+
+  public static Type getType(String varType){
+    switch(varType){
+      case "void":
+        return Type.VOID;
+      case "int":
+        return Type.INT;
+      case "boolean":
+        return Type.BOOLEAN;
+      default:
+        return Type.VOID;
+    }
+  }
+
   String symbol;
   Type type;
 
 
-  SymbolType(String symbol,Type type){
+  SymbolType(String symbol,String varType){
     this.symbol = symbol;
-    this.type = type;
-  }
+    this.type = getType(varType);
+    }
 }
