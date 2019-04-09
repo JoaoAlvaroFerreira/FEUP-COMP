bf2a2a0e8f193ba4a0151e2c16d4c2c40291c52c
Tiago Ribeiro <tiago7b27@gmail.com>
2019-04-09 12:01:03 +0100

    commments

--- SymbolTable.java
+++ SymbolTable.java
@@ -5,7 +5,10 @@ public class SymbolTable{
     static String[] variables = {"int", "int[]", "bool"};
     ArrayList<SymbolTableEntry> entries = new ArrayList<SymbolTableEntry>();
 
-    
+    //fazer construtor symbol table
+
+    //fazer prints symbol table
+
     int getValueEntry(int i) {
     	return entries.get(i).getValue();
     }
