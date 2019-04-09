dd508ff84dd60c6dff9c9a3e80cd7380b53ae843
JoaoAlvaroFerreira <JoaoAlvaroFerreira@FIREBOY.localdomain>
2019-04-09 11:55:55 +0100

    aula

--- SymbolTableEntry.java
+++ SymbolTableEntry.java
@@ -6,7 +6,9 @@ public class SymbolTableEntry{
     
 	SymbolType symboltype;
 	SymbolType returnDescriptor;
-	ArrayList<Node> nodelist;
+	ArrayList<SimpleNode> nodelist;
+	ArrayList<SymbolType> params;
+	ArrayList<SimpleNode> aux;
 	
 	
 	public SymbolTableEntry(SimpleNode Node) {
@@ -15,10 +17,16 @@ public class SymbolTableEntry{
 		SymbolType symboltype = new SymbolType(Node.symbol, (String)Node.value); //key, value
 		
 		for(int i = 0; i < Node.jjtGetNumChildren(); i++ ) {
-		nodelist.add(Node.jjtGetChild(i));
+		nodelist.add((SimpleNode)Node.jjtGetChild(i));
 		}		
 		
-		returnDescriptor = new SymbolType(nodelist.get(0).)
+		returnDescriptor = new SymbolType(nodelist.get(0).getSymbol(), (String)nodelist.get(0).getValue());
+		
+		
+		for(int j = 0; j < nodelist.get(1).jjtGetNumChildren(); j++ ) {
+			aux.add((SimpleNode)nodelist.get(1).jjtGetChild(j));
+			params.add(new SymbolType(aux.get(j).getSymbol(), (String)aux.get(j).getValue()));
+			}		
 		
 	}
 	
