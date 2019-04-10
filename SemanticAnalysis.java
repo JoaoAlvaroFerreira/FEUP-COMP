6435dd291dba2ac04834457e48135c1b91dca0c7
MarianaFerrDias <MarianaFerrDias@github.com>
2019-04-10 10:38:02 +0100

    added semantic analysis

---
+++ SemanticAnalysis.java
@@ -0,0 +1,25 @@
+
+public class SemanticAnalysis{
+
+  enum Type{
+    INT,
+    INT_ARR,
+    BOOLEAN,
+    VOID;
+  }
+
+ // public Object visit(SimpleNode node, SymbolTable data) {
+    // String name = (String)node.jjtGetValue();
+
+    // consultar symboltable, ver se nome existe
+
+    // se sim, retorna TYPE do node
+    // se não, retorna TYPE.Erro
+
+    //Type identifierType = node.jjtGetChild(0).visit(this, data);
+
+    //if (identifierType == ERRO) {
+      // retorna TYPE.Erro
+    //}
+  // }
+}
