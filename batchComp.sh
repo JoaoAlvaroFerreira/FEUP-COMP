3da6924dd56a565433e1befe4a0efcff2527ac09
Tiago Ribeiro <up201605619@fe.up.pt>
2019-04-28 04:05:24 +0100

    batchComp now executes jasmin and executes program

--- batchComp.sh
+++ batchComp.sh
@@ -7,3 +7,5 @@ echo -----------------------------------
 java Main Fac.jmm
 echo -----------------------------------
 java -jar jasmin-2.4/jasmin.jar Fac.j
+echo -----------------------------------
+java Fac
